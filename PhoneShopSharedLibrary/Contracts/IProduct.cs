168830f660965eb6ab7e69e845545c31b48e8e18
Netcode-Hub <Rastinga1995@hotmail.com>
2023-12-03 08:18:31 +0000

    Add project files.

---
+++ PhoneShopSharedLibrary/Contracts/IProduct.cs
@@ -0,0 +1,11 @@
+﻿using PhoneShopSharedLibrary.Models;
+using PhoneShopSharedLibrary.Responses;
+namespace PhoneShopSharedLibrary.Contracts
+{
+    public interface IProduct
+    {
+        Task<ServiceResponse> AddProduct(Product model);
+        Task<List<Product>> GetAllProducts(bool featuredProducts);
+
+    }
+}
